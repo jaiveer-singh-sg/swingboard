bc3b125d7afcf76688b33e9a8be8778f4e17d2a2
Jaiveer Singh <dropbox4you101@gmail.com>
2026-07-02 21:04:29 +0800

    Add global variables for dashboard functionality

---
+++ js/globals.js
@@ -0,0 +1,13 @@
+/**********************************************************************
+    GLOBAL VARIABLES
+**********************************************************************/
+
+let dashboardData = [];
+let uploadedCSV = "";
+
+let currentSort = "score";
+let currentFilter = "all";
+let searchText = "";
+
+let charts = {};
+let chartInstances = {};
